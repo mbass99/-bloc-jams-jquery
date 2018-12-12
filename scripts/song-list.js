3101871955fc0b27e2c2fdb46ed4f4a603e10bbe
Matt Bass <mattbass99@gmail.com>
2018-12-12 15:58:18 -0800

    jerry assist

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -16,8 +16,9 @@
 
         song.element.on('click', event => {
             player.playPause(song);
+            $('button#play-pause').attr('playState', player.playState);
         });
 
-        $('#song-list').append(song.element);
+        $('button#play-pause').attr('playState', player.playState);
     });
 }
