f0541db53efbb3ea8d9f01e82f0c229ed6fb37f9
Matt Bass <mattbass99@gmail.com>
2018-12-11 23:33:38 -0800

    song list

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -3,6 +3,5 @@
     $('img#album-cover-art').attr('src', album.albumArtUrl);
     $('.artist').text(album.artist);
     $('#release-info').text(album.releaseInfo);
-    //$('#album-details').text('src', album.albumDetails);
-    //$('#album-info').(album.albumInfo);
+
 }
