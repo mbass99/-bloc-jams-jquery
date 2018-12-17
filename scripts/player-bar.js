a4892ac4d0adecf77867932fde51d318adae7b71
Matt Bass <mattbass99@gmail.com>
2018-12-16 16:16:27 -0800

    slack help 2

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -35,26 +35,25 @@
         layPauseAndUpdate(previousSong);
     });
 
-   $('#time-control input').on('input', function (event) {
-       player.skipTo(event.target.value);
+ $('#time-control input').on('input', function (event) {
+     player.skipTo(event.target.value);
    });
-
-    setInterval(() => {
-        if (player.playState !== 'playing') {
-            return;
-        }
-    const currentTime = player.getTime();
+    
+    $('#volume-control input').on('input', function(event){
+        player.setVolume(event.target.value);
+    });
+    
+    
+    setInterval( () => {
+        if (player.playState !== 'playing') { return; } 
+        const currentTime = player.getTime();
      const duration = player.getDuration();
      const percent = (currentTime / duration) * 100;
-        $('#time-control .current-time').text( currentTime );
-         $('#time-control .total-time').text( player.currentTime(DURATION) );
-      $('#time-control input').val(percent);
+         $('#time-control .current-time').text( currentTime );
+        $('#time-control input').val(percent);
+        $('#time-control .total-time').text( currentTime (duration) );
+   }, 1000);
 
-    $('#volume-control input').on('input', function (event) {
-        player.setVolume(event.target.value);
-    })
-};
+}
                 
-                
-                }
-                
+
